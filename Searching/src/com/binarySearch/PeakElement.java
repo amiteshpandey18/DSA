dca2d3674e977c3b262fcc0ef17612a1db4378c4
Amitesh Pandey <amiteshpandey7050@gmail.com>
2026-02-14 23:51:19 +0530

    2nd day DSA

--- Searching/src/com/binarySearch/PeakElement.java
+++ Searching/src/com/binarySearch/PeakElement.java
@@ -7,7 +7,7 @@ public class PeakElement {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter size of an Array");
         int n = sc.nextInt();
-        int arr[] = new int[n];
+        int[] arr = new int[n];
 
         System.out.println("Enter Element");
         for (int i = 0; i <= arr.length - 1; i++) {
